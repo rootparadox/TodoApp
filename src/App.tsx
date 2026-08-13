3f00dcafae0796dce03269a83b37b56a46b1a857
= <hizenberg917@gmail.com>
2026-08-13 21:21:35 +0330

    Finished work in every device sizes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import Header from "./Components/Header";
 import Todo from "./Components/TodoCards/TodoCard";
 const App = () => {
   return (
-    <div className="p-5 my-10 w-full h-screen flex flex-col gap-6">
-      <Header />
-      <Todo />
+    <div className="min-h-screen md:bg-stone-50 flex md:items-center justify-center">
+      <div className="p-5 my-10 w-full bg-white flex flex-col gap-6 md:w-[80%] md:shadow md:max-w-5xl md:min-h-[70vh] md:rounded-2xl">
+        <Header />
+        <Todo />
+      </div>
     </div>
   );
 };
